9e32ced799f2c2d6d9cd422d02ec93a8dd83559b
Bribo <217203027@mycput.ac.za>
2021-03-25 23:02:37 +0200

    Modifications has been made

--- src/main/java/za/ac/cput/subjectreport/Report.java
+++ src/main/java/za/ac/cput/subjectreport/Report.java
@@ -10,5 +10,42 @@ package za.ac.cput.subjectreport;
  * @author Breyton Ernstzen - 217203027
  */
 public class Report {
-    
+    private String studName,studSurname,course;
+    private long studNum;
+    private int averageMark;
+      
+    public void setName(String name){
+        this.studName = name;
+    }
+    public String getName(){
+        return studName;
+    }
+    public void setSurname(String surname){
+        this.studSurname = surname;
+    }
+    public String getSurname(){
+        return studSurname;
+    }
+    public void setStudentNumber(long studentNumber){
+        this.studNum = studentNumber;
+    }
+    public long getStudentNumber(){
+        return studNum;
+    }
+    public void setCourse(String course){
+        this.course = course;
+    }
+    public String getCourse(){
+        return course;
+    }
+    public void setAverageMark(int averageMark){
+        this.averageMark = averageMark;
+    }
+    public int getAverageMark(){
+        return averageMark;
+    }
+    public String toString(){
+        String str = String.format("");
+        return str;
+    }
 }
