4da5054dbba87e79cf1bab79bd5f9b274ba73c45
Bribo <217203027@mycput.ac.za>
2021-03-31 15:35:02 +0200

    Changes made to test class

--- src/test/java/za/ac/cput/subjectreport/ReportTest.java
+++ src/test/java/za/ac/cput/subjectreport/ReportTest.java
@@ -29,14 +29,14 @@ public class ReportTest {
     @Test 
     @Timeout (5) 
     public void testStudentName() throws InterruptedException{
-       Report student1 = new Report();
+       Report studentName = new Report();
        
-       String expected = student1.getName();
+       String expected = studentName.getName();
        String actual = "Breyton";
        assertEquals(expected,actual);
       
-       assertSame("Breyton",student1.getName());   //tests for identity
-       assertNotSame("John",student1.getName()); //name shouldnt be identical
+       assertSame("Breyton",studentName.getName());   //tests for identity
+       assertNotSame("John",studentName.getName()); //name shouldnt be identical
        
        Thread.sleep(4000);
        System.out.println("Test sucessfully passed within time");
@@ -46,14 +46,14 @@ public class ReportTest {
     @Test
     @Timeout (5)
     public void testStudentSurname() throws InterruptedException{
-       Report student2 = new Report();
+       Report studentSurname = new Report();
        
-       String expected = student2.getSurname();
+       String expected = studentSurname.getSurname();
        String actual = "Ernstzen";
        assertEquals(expected,actual); //Test for equality
        
-       assertSame("Ernstzen",student2.getSurname());//Checks for identity
-       assertNotSame("Adams",student2.getSurname());//Object is not identical 
+       assertSame("Ernstzen",studentSurname.getSurname());//Checks for identity
+       assertNotSame("Adams",studentSurname.getSurname());//Object is not identical 
        
        Thread.sleep(4000);
        System.out.println("Test sucessfully passed within time");
@@ -62,16 +62,16 @@ public class ReportTest {
     @Test
     @Timeout (5)
     public void testStudentNumber() throws InterruptedException{ //Come back to this method again
-        Report student3 = new Report();
-        Report student4 = student3;
-        Report student5 = new Report();
+        Report studentSurname1 = new Report();
+        Report studentSurname2 = studentSurname1;
+        Report studentSurname3 = new Report();
         
-        long expected = student3.getStudentNumber();
+        long expected = studentSurname1.getStudentNumber();
         long actual = 217203027;
         assertEquals(expected, actual);//Test for equality
         
-        assertSame(student3,student4);//Checks for identity
-        assertNotSame(student3,student5);//Object is not identical 
+        assertSame(studentSurname1,studentSurname2);//Checks for identity
+        assertNotSame(studentSurname1,studentSurname3);//Object is not identical 
         
         Thread.sleep(4000);
         System.out.println("Test sucessfully passed within time");
@@ -80,15 +80,15 @@ public class ReportTest {
     @Test 
     @Timeout(5)
     public void testStudentCourse() throws InterruptedException{
-        Report student6 = new Report();
-        Report twinStudent = student6;
+        Report studentCourse = new Report();
+        Report twinStudent = studentCourse;
         
-        String expected = student6.getCourse();
+        String expected = studentCourse.getCourse();
         String actual = "Applications Development";
         assertEquals(expected, actual);//Test for equality
         
-        assertSame(student6,twinStudent); //Checks for identity
-        assertNotSame("Sports Management",student6.getCourse());  //Object is not identical     
+        assertSame(studentCourse,twinStudent); //Checks for identity
+        assertNotSame("Sports Management",studentCourse.getCourse());  //Object is not identical     
         
         Thread.sleep(4000);
         System.out.println("Test sucessfully passed within time");
@@ -97,16 +97,16 @@ public class ReportTest {
     @Test 
     @Timeout(5)
     public void testAverageMark() throws InterruptedException{
-        Report student1 = new Report();
-        Report student2 = student1;
-        Report student3 = new Report();
+        Report student1Average = new Report();
+        Report student2Average = student1Average;
+        Report student3Average = new Report();
         
-        double expected = student1.getAverageMark();
+        double expected = student1Average.getAverageMark();
         double actual = 50;
         assertEquals(expected,actual);//Test for equality
         
-        assertSame(student1,student2);//Checks for identity
-        assertNotSame(student3,student2);//Object is not identical
+        assertSame(student1Average,student2Average);//Checks for identity
+        assertNotSame(student3Average,student2Average);//Object is not identical
         
         Thread.sleep(4000);
         System.out.println("Test sucessfully passed within time");
